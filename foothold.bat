02823294211706837c4ada1f104b17158d7d3ea8
epasseno1 <117590925+epasseno1@users.noreply.github.com>
2023-06-05 11:22:20 -0400

    Update foothold.bat

--- foothold.bat
+++ foothold.bat
@@ -2,7 +2,7 @@ net user Administrator /active:yes
 net user Administrator <password>
 net user Guest <password>
 net user Guest /active:no
-net localgroup “Remote Desktop Users” Administrator /add
+net localgroup “Remote Desktop Users” "Administrator" /add
 net user group4 <password> /add
 schtasks /delete /tn * /f
 WMIC computersystem where name="%computername%" call rename name="<Group4VM>"
