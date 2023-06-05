f0b0ff04730cff19dcc1dd8b43f8216d332a1db3
epasseno1 <117590925+epasseno1@users.noreply.github.com>
2023-06-05 10:05:13 -0400

    Update foothold.bat

--- foothold.bat
+++ foothold.bat
@@ -4,7 +4,6 @@ net user Guest <password>
 net user Guest /active:no
 net localgroup “Remote Desktop Users” Administrator /add
 net user group4 <password> /add
-netsh firewall reset
 schtasks /delete /tn * /f
 WMIC computersystem where name="%computername%" call rename name="<Group4VM>"
 shutdown /r /t 0
